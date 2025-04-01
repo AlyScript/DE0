519066ac30b7ee09795312a29d47b2c30b6a80ad
Adam <adamaly93@gmail.com>
2025-04-01 08:43:07 +0100

    Refactor LED toggle logic and output assignment in playing module
    We should assign the output LED *after* the logic for detecting the falling edge of the switch, hence is outside of the always block, since whatever r_LED_1 is, o_LED_1 should be latched at that state.

--- Playing/playing.v
+++ Playing/playing.v
@@ -1,10 +1,10 @@
 module playing(
     input   clk,		    // 50 mhz
-    input   i_Switch_1    
+    input   i_Switch_1,    
     output  o_LED_1
 );
 
-    // Internal Signals
+    // Internal Signal / Registers
 	reg r_LED_1;
 	reg r_Switch_1;         // We use this to detect falling edge of the switch
 
@@ -17,7 +17,8 @@ module playing(
             r_LED_1 <= ~r_LED_1;    // Toggle the LED state
         end
         
-        assign o_LED_1 = r_LED_1;   // Assign the LED state to the output (blocking since we want to do it after r_LED_1 is updated)
 	end
-
+    
+    assign o_LED_1 = r_LED_1;       // Output the LED state
+    
 endmodule
