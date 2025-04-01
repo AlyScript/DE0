1e1e5e55ab30109d4a13b2b2ddddedbc7cee27a1
Adam <adamaly93@gmail.com>
2025-04-01 08:37:07 +0100

    Functionality to toggle LED on switch falling edge

--- Playing/playing.v
+++ Playing/playing.v
@@ -1,31 +1,23 @@
 module playing(
-    input clk,		// 50 mhz
-	input rst, 	// reset signal
-	output reg led
+    input   clk,		    // 50 mhz
+    input   i_Switch_1    
+    output  o_LED_1
 );
 
-	reg[31:0] counter;
-	
-	always @ (posedge clk, posedge rst)
-	begin
+    // Internal Signals
+	reg r_LED_1;
+	reg r_Switch_1;         // We use this to detect falling edge of the switch
 
-		if (rst)
-		begin
-			counter <= 0;
-			led <= 0;
-		end
-		else
-		begin
-			if (counter <= 25000000)
-			begin
-				counter <= counter + 1;
-			end
-			else
-			begin
-				counter <= 0;
-				led <= ~led;
-			end
-		end
+	always @ (posedge clk)
+	begin
+        r_Switch_1 <= i_Switch_1;   // Store the current state of the switch for the next clock cycle
+
+        if (i_Switch_1 == 1'b0 && r_Switch_1 == 1'b1) // Detect falling edge of the switch
+        begin
+            r_LED_1 <= ~r_LED_1;    // Toggle the LED state
+        end
+        
+        assign o_LED_1 = r_LED_1;   // Assign the LED state to the output (blocking since we want to do it after r_LED_1 is updated)
 	end
 
 endmodule
